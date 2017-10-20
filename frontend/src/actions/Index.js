29110854c3ff25f667aa73d824a7a22bd53a92c3
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-19 19:56:34 -0500

    Action

---
+++ frontend/src/actions/Index.js
@@ -0,0 +1,16 @@
+
+export const CreatePost = 'CreatePost';
+export const GetPosts = 'GetPosts';
+export const DeletePost = 'DeletePost';
+export const EditPost = 'EditPost';
+export const GetPost = 'GetPost';
+export const CreateComment = 'CreateComment';
+export const EditComment = 'EditComment';
+export const DeleteComment = 'DeleteComment';
+export const UpVote = 'UpVote';
+export const DownVote = 'DownVote';
+export const GetCategories = 'GetCategories';
+export const GetCategoryPost = 'GetCategoryPost';
+export const GetComments = 'GetComments';
+export const UpVoteComment = 'UpVoteComment';
+export const DownVoteComment = 'DownVoteComment';
