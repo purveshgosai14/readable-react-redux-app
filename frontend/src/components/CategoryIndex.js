41376f45097aae71e8bb360b0dae746631d74a24
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-24 23:47:32 -0500

    Update Component
    Added require fields on JSX code and link up the page.
    Also exported to redux form

--- frontend/src/components/CategoryIndex.js
+++ frontend/src/components/CategoryIndex.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getAllCategories } from '../actions/CategoryAction';
+import _ from 'lodash';
 import { Link } from 'react-router-dom';
-
+import './header.css';
 
 class CategoryIndex extends Component {
+    componentDidMount() {
+        this.props.getAllCategories();
+    }
 
+    renderCategories() {
+        return _.map(this.props.categories, category => {
+            return (
+                <li key={category.name} className="nav-item">
+                    <Link to={`/${category.path}`} className="nav-link">
+                        {category.name}
+                    </Link>
+                </li>
+            );
+        });
+    }
 
     render() {
         return (
             <div className="Header">
-                <div className='Left'>
-                    <h1 className='Logo'>
+                <div className="all-categories">
+                    <h1 className="left-readable">
                         <Link to='/'>Readable</Link>
                     </h1>
                 </div>
@@ -24,6 +41,12 @@ class CategoryIndex extends Component {
             </div>
         );
     }
-
 }
-export default CategoryIndex
+
+function mapStateToProps({ categories }) {
+    return {
+        categories
+    }
+}
+
+export default connect(mapStateToProps, { getAllCategories })(CategoryIndex);
