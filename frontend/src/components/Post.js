71dc03c24358c19ef6daaef0c670f13700379470
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-30 18:11:23 -0500

    Revert "fix: display total comments on post"
    This reverts commit 24bc2f9806ae3e1a8c61c52d380588d97d5f659c.

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,109 +1,89 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import moment from 'moment';
-import {getPosts, deletePost} from '../actions/PostAction';
-import { upVote, downVote, } from '../actions/VoteAction';
-import { getComments } from '../actions/CommentAction';
-import _ from 'lodash';
+    import React, { Component } from 'react';
+    import { Link } from 'react-router-dom';
+    import { connect } from 'react-redux';
+    import moment from 'moment';
+    import {getPosts, deletePost} from '../actions/PostAction';
+    import { upVote, downVote, } from '../actions/VoteAction';
 
-class Post extends Component {
+    class Post extends Component {
 
-    constructor(props){
-        super(props);
-        this.state ={
-            totalComments : 0
+        onUpVote(id) {
+            this.props.upVote(id, () => {
+                this.props.getPosts();
+            });
         }
-    }
 
-    onUpVote(id) {
-        this.props.upVote(id, () => {
-            this.props.getPosts();
-        });
-    }
+        onDownVote(id) {
+            this.props.downVote(id, () => {
+                this.props.getPosts();
+            });
+        }
 
-    onDownVote(id) {
-        this.props.downVote(id, () => {
-            this.props.getPosts();
-        });
-    }
+        onDeletePost(id) {
+            this.props.deletePost(id, () => {
+                this.props.getPosts();
+            });
+        }
 
-    onDeletePost(id) {
-        this.props.deletePost(id, () => {
-            this.props.getPosts();
-        });
-    }
+        render() {
+            const { id, title, body, author, timestamp, category, voteScore, isDetail, comments } = this.props;
+            return (
+                <div className="text-left" >
+                    <div className="row">
+                        <h2>
+                            <strong>
+                                {isDetail
+                                    ? title
+                                    : <Link to={`/${category}/${id}`}>
+                                        {title}
+                                    </Link>
+                                }
+                            </strong>
+                        </h2>
+                    </div>
 
-    componentWillMount(){
-        this.props.getComments(this.props.id).then((res)=>{
-            this.setState({totalComments: _.size(res.payload.data)})
-        })
-    }
+                    <div className="row">
+                        <h4>
+                            {body}
+                        </h4>
+                    </div>
 
-
-    render() {
-        const {totalComments} = this.state;
-        const { id, title, body, author, timestamp, category, voteScore, isDetail } = this.props;
-        let comments = this.props.comments !== undefined ? this.props.comments : totalComments;
-
-        return (
-            <div className="text-left" >
-                <div className="row">
-                    <h2>
-                        <strong>
-                            {isDetail
-                                ? title
-                                : <Link to={`/${category}/${id}`}>
-                                    {title}
-                                </Link>
-                            }
-                        </strong>
-                    </h2>
-                </div>
-
-                <div className="row">
-                    <h4>
-                        {body}
-                    </h4>
-                </div>
-
-                <div className="row row-icon">
-                    <i>
-                        <i className="material-icons md-">face</i>
-                        <span className="author">
-                            {author}
-                        </span>
-                    </i>
-
-                    <i className="fa fa-arrows-v">
-                        <i className="vote-score"> voteScore: {voteScore} </i>
-                    </i>
-
-                    <i className="fa fa-calendar">
-                        {moment(timestamp).format('LLLL')}
-                    </i>
-
-                    <i>Category : <span className="label label-info">{category}</span></i>
-                    {comments ? (<i className="icon-comment"><strong>{comments} Comments </strong></i>) : <span></span>}
-
-                    <div className="all-buttons">
-                        <i className="fa fa-thumbs-up fa-2x" onClick={this.onUpVote.bind(this, id)}>
+                    <div className="row row-icon">
+                        <i>
+                            <i className="material-icons md-">face</i>
+                            <span className="author">
+                                {author}
+                            </span>
                         </i>
-                        <i className="fa fa-thumbs-down fa-2x" onClick={this.onDownVote.bind(this, id)}>
+
+                        <i className="fa fa-arrows-v">
+                            <i className="vote-score"> voteScore: {voteScore} </i>
                         </i>
-                        <i title="Delete Post" className="material-icons delete-button" onClick={this.onDeletePost.bind(this, id)}>
-                            delete
+
+                        <i className="fa fa-calendar">
+                            {moment(timestamp).format('LLLL')}
                         </i>
-                        <Link title="Edit Post" to={`/${category}/${id}/edit`}>
-                            <i className="material-icons">
-                                mode_edit
+
+                        <i>Category : <span className="label label-info">{category}</span></i>
+                        {comments ? (<i className="icon-comment">{comments} Comments</i>) : <span></span>}
+
+                        <div className="all-buttons">
+                            <i className="fa fa-thumbs-up fa-2x" onClick={this.onUpVote.bind(this, id)}>
                             </i>
-                        </Link>
+                            <i className="fa fa-thumbs-down fa-2x" onClick={this.onDownVote.bind(this, id)}>
+                            </i>
+                            <i title="Delete Post" className="material-icons delete-button" onClick={this.onDeletePost.bind(this, id)}>
+                                delete
+                            </i>
+                            <Link title="Edit Post" to={`/${category}/${id}/edit`}>
+                                <i className="material-icons">
+                                    mode_edit
+                                </i>
+                            </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
-}
-
-export default connect(null, { upVote, downVote, getPosts, deletePost, getComments })(Post);
+    export default connect(null, { upVote, downVote, getPosts, deletePost })(Post);
