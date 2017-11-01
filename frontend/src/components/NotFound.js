7fe0a2969d21c0ccdd8cbf869faa7912936e7b49
Purvesh Gosai <puri.ui369@gmail.com>
2017-11-01 02:51:35 -0500

    fix: Bug fix for notfound component

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () =>
-    <div>
-        <h3>404 page not found</h3>
-        <p>We are sorry but the page you are looking for does not exist.</p>
-    </div>
+const NotFound = () => {
+    return (
+        <div>
+            <h3>404 page not found</h3>
+            <h4>We are sorry but the page you are looking for does not exist, please <Link to='/'>click here </Link> to go back</h4>
+        </div>
+    )
+};
 
 export default NotFound;
